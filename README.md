b023b1f7d4dd0a8fd7ace304504d64393b09e7e5
Eduardo Dias Gusmão <edg@outlook.com>
2023-10-01 03:18:53 +0000

    add `lint:check` and `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tabnews
-News Feed 
+
+News Feed
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-  return <h1>Codespaces is awesome!</h1>
+function Home() {
+  return <h1>Codespaces is awesome!</h1>;
 }
 
 export default Home;
